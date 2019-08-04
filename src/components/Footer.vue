f0a79ba6c2bc19014c4f406294b0ab4b3032764e
carlos Llorente <llorenteyuste@hotmail.com>
2019-08-04 23:07:04 +0200

    Sticky footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -16,8 +16,11 @@ export default {
   background-color: #8dd5b5;
   padding: 10px;
   opacity: 0.6;
-  margin: 0 auto;
-  margin-top: 20px !important;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
 }
 .logo {
   width: 2%;
